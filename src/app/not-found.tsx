ae69a7e9a791e8ad5eb8bf659d011d01207e0f80
kingsuk <kingsuk@github.com>
2024-03-09 08:34:34 +0530

    fix some style issue

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ const NotFound = () => {
     <section className="bg-white w-full h-full flex justify-center items-center">
       <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div className="mx-auto max-w-screen-sm text-center">
-          <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 text-primary-500">
+          <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 text-blue-500">
             404
           </h1>
           <p className="mb-4 text-3xl tracking-tight font-bold md:text-4xl text-gray-900">
@@ -17,7 +17,7 @@ const NotFound = () => {
           </p>
           <Link
             href="/"
-            className="inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center my-4 bg-primary-900"
+            className="inline-flex text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center my-4 bg-primary-900"
           >
             Back to Homepage
           </Link>
