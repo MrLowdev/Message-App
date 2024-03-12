ddb14d405ddaaab83ee8146da5432c4704c1e105
kingsuk <kingsuk@github.com>
2024-03-12 09:52:49 +0530

    fix style and redrict url

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -16,8 +16,8 @@ const NotFound = () => {
             the home page.
           </p>
           <Link
-            href="/"
-            className="inline-flex text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center my-4 bg-primary-900"
+            href="/conversations"
+            className="inline-flex text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center my-4"
           >
             Back to Homepage
           </Link>
